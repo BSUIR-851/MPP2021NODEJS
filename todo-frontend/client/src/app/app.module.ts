73b33ebeb47c410b63ab715278909b21102d0577
yuramorozov01 <yura_morozov01@mail.ru>
2021-02-21 00:00:56 +0300

    feat: add login in auth service

--- todo-frontend/client/src/app/app.module.ts
+++ todo-frontend/client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -22,8 +23,10 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- todo-frontend/client/src/app/login-page/login-page.component.ts
+++ todo-frontend/client/src/app/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
 	selector: 'app-login-page',
@@ -10,17 +15,42 @@ export class LoginPageComponent implements OnInit {
 
 	form: FormGroup;
 
-	constructor() { }
+	aSub: Subscription;
+
+	constructor(private auth: AuthService,
+				private router: Router,
+				private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		this.form = new FormGroup({
 			email: new FormControl(null, [Validators.required, Validators.email]),
-			password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+			password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+		});
+
+		this.route.queryParams.subscribe((params: Params) => {
+			if (params['registered']) {
+				// you have been registered and you can login
+			} else if (params['accessDenied']) {
+				// you have to login
+			}
 		});
 	}
 
+	ngOnDestroy(): void {
+		if (this.aSub) {
+			this.aSub.unsubscribe();
+		}
+	}
+
 	onSubmit() {
-		
+		this.form.disable();
+		this.aSub = this.auth.login(this.form.value).subscribe(
+			() => this.router.navigate(['/active']),
+			error => {
+				console.warn(error);
+				this.form.enable();
+			}
+		);
 	}
 
 }
