f2100eda22dff9b6bf5e0c8955b78c0479090bec
yuramorozov01 <yura_morozov01@mail.ru>
2021-02-28 23:27:10 +0300

    feat: some design changes

--- todo-frontend/client/src/app/active-page/active-page.component.ts
+++ todo-frontend/client/src/app/active-page/active-page.component.ts
@@ -15,33 +15,24 @@ import { MaterialService } from '../shared/classes/material.service';
 })
 export class ActivePageComponent implements OnInit {
 
-	form: FormGroup;
 	activeTasks$: Observable<Task[]>;
 
 	constructor(private tasksService: TasksService) { }
 
 	ngOnInit(): void {
-		this.form = new FormGroup({
-			choosedTask: new FormControl('', Validators.required),
-		});
-
 		this.activeTasks$ = this.tasksService.getAllActive();
 	}
 
-	onSubmit() {
+	onClick(event: Event, taskId: string) {
 		let obs$;
-		this.form.disable();
-		let taskId = this.form.value.choosedTask;
 		if (taskId) {
 			obs$ = this.tasksService.complete(taskId);
 			obs$.subscribe(
 				(task: Task) => {
 					MaterialService.toast(`Task ${task.description} has been completed successfully`);
-					this.form.enable();
 				},
 				error => {
 					MaterialService.toast(error.error.message);
-					this.form.enable();
 				}
 			);
 		}
