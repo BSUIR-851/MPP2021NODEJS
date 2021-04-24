24700ab950c00c9f1b217f64a50f60a66d482b72
yuramorozov01 <yura_morozov01@mail.ru>
2021-04-24 18:07:17 +0300

    feat: add websocket echo broadcast

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,7 +1,29 @@
+
 const app = require('./app.js');
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const http = require('http');
+const server = http.createServer();
+
+const ws = require('ws');
+const WSServer = ws.Server;
+const wss = new WSServer({ server: server });
+
+server.on('request', app);
+
+wss.on('connection', (socket, req) => {
+	console.log(req.socket.remoteAddress + ': connected');
+	socket.on('message', (msg) => {
+		console.log(req.socket.remoteAddress + ': ' + msg);
+		wss.clients.forEach((client) => {
+			if (client !== socket && client.readyState === ws.OPEN) {
+				client.send(msg);
+			}
+		});
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Server has been started on port: ${ port }`)
 });
 
