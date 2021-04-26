ff0576336dec319bb60b9c23ff50d670a16350e3
yuramorozov01 <yura_morozov01@mail.ru>
2021-04-27 00:23:38 +0300

    feat: add basic home page for included game

--- todo-frontend/client/src/app/game-page/game-page.component.ts
+++ todo-frontend/client/src/app/game-page/game-page.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { WebSocketService } from '../shared/services/websocket/websocket.service';
+import { IWebSocketMessage } from '../shared/services/websocket/websocket.interfaces';
+import { WS } from './game.events';
+
+import { MaterialService } from '../shared/classes/material.service';
+
+export interface IMessageGameData {
+  mode: string;
+  board: Array<boolean>;
+  obstacles: Array<object>;
+  fruit: object;
+  snake: object;
+  score: number;
+}
+
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
@@ -7,9 +24,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePageComponent implements OnInit {
 
-  constructor() { }
+	messagesGameData$: Observable<IMessageGameData[]>;
+	EVENTS: object = WS;
+
+  constructor(private wsService: WebSocketService) { }
 
   ngOnInit(): void {
+  	this.messagesGameData$ = this.wsService.on<IMessageGameData[]>(WS.ON.GAME_DATA);
   }
-
 }
---
+++ todo-frontend/client/src/app/game-page/game.events.ts
@@ -0,0 +1,12 @@
+export const WS = {
+	ON: {
+		START: 'start',
+		END: 'end',
+		GAME_DATA: 'game-data',
+	},
+	SEND: {
+		START: 'start',
+		END: 'end',
+		GAME_DATA: 'game-data',
+	}
+}
